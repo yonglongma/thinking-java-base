e4c1a05cc76d0ff44bb39c31f24615a5da3f18ea
mayonglong <mayonglong123@aliyun.com>
2017-05-08 14:16:18 +0800

    init

---
+++ src/main/java/com/yonglong/object/Equivalence.java
@@ -0,0 +1,24 @@
+package com.yonglong.object;
+
+/**
+ * @author MaYongLong
+ * @date 2017/5/8.
+ */
+public class Equivalence {
+    public static void main(String[] args){
+        Integer n1 = new Integer(47);
+        Integer n2 = new Integer(47);
+        System.out.println(n1 == n2); //false
+        System.out.println(n1.equals(n2)); //true
+
+        Value v1 = new Value();
+        Value v2 = new Value();
+        v1.i = v2.i = 100;
+        System.out.println(v1 == v2); //false
+        System.out.println(v1.equals(v2)); //false
+    }
+
+}
+class Value{
+    int i;
+}
